358841a54d4cab168428d9f7b55403315138ca49
Polina <ipolina33@gmail.com>
2020-12-15 21:12:30 +0500

    finished layout of the large version

---
+++ src/components/blocks/starsRate/StarsRate.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="starsRate__container">
+    <p class="starsRate__title">{{ title }}</p>
+    <div class="starsRate__starsContainer">
+      <input
+        type="radio"
+        :name="`${title}-rating`"
+        class="starsRate__starInput"
+        :id="`${title}-rating-5`"
+        v-bind:value="5"
+        :checked="value === 5"
+      />
+      <label
+        class="starsRate__star"
+        :for="`${title}-rating-5`"
+        v-bind:value="5"
+      ></label>
+      <input
+        type="radio"
+        :name="`${title}-rating`"
+        class="starsRate__starInput"
+        :id="`${title}-rating-4`"
+        v-bind:value="4"
+        :checked="value === 4"
+      />
+      <label
+        class="starsRate__star"
+        :for="`${title}-rating-4`"
+        v-bind:value="4"
+      ></label>
+      <input
+        type="radio"
+        :name="`${title}-rating`"
+        class="starsRate__starInput"
+        :id="`${title}-rating-3`"
+        v-bind:value="3"
+        :checked="value === 3"
+      />
+      <label
+        class="starsRate__star"
+        :for="`${title}-rating-3`"
+        v-bind:value="3"
+      ></label>
+      <input
+        type="radio"
+        :name="`${title}-rating`"
+        class="starsRate__starInput"
+        :id="`${title}-rating-2`"
+        v-bind:value="2"
+        :checked="value === 2"
+      />
+      <label
+        class="starsRate__star"
+        :for="`${title}-rating-2`"
+        v-bind:value="2"
+      ></label>
+      <input
+        type="radio"
+        :name="`${title}-rating`"
+        class="starsRate__starInput"
+        :id="`${title}-rating-1`"
+        v-bind:value="1"
+        :checked="value === 1"
+      />
+      <label
+        class="starsRate__star"
+        :for="`${title}-rating-1`"
+        v-bind:value="1"
+      ></label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    rate: {
+      type: Number,
+      required: false
+    }
+  },
+  data() {
+    return {
+      value: 1
+    };
+  },
+  mounted() {
+    this.value = this.rate | 0;
+  },
+  watch: {
+    value: function() {
+      console.log(this.value);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/components/blocks/starsRate/starsRate.scss";
+</style>
---
+++ src/components/review/Review.vue
@@ -0,0 +1,96 @@
+<template>
+  <section class="review__container">
+    <header class="review__header">
+      <h2 class="review-header__text">Мой отзыв</h2>
+      <button class="review-header__closeReview" type="button"></button>
+    </header>
+    <hr class="review__decorateLine" />
+    <div class="review__wrapper">
+      <div class="review__authorInfoContainer">
+        <img :src="authorPhotoUrl" alt="photo" class="review__authorPhoto" />
+        <div class="review__authorInfo">
+          <h2 class="review__reviewTitle">Фоточки в свадебном платьице</h2>
+          <p class="review__authorName">Алена Смирнова</p>
+        </div>
+      </div>
+      <form action="submit" @submit.prevent class="review__form">
+        <div class="review-form__starsContainer">
+          <StarsRate
+            v-for="(elem, index) of starsInfo"
+            :key="index"
+            :title="elem.title"
+            :rate="elem.rate"
+          />
+        </div>
+        <div class="review-form__commentContainer">
+          <textarea
+            class="review-form__comment"
+            name="review-form__comment"
+            id="review-form__textarea"
+            placeholder="Комментарий "
+          ></textarea>
+          <label for="review-form__textarea" class="review-form__counter">
+            12/500
+          </label>
+        </div>
+        <div class="review-form__photoContainer">
+          <button class="review-form__addPhotoBtn" type="button"></button>
+          <div
+            class="review-form__photoCard"
+            v-for="(photo, index) of addedPhotos"
+            :key="index"
+          >
+            <img :src="photo" alt="photo" class="review-form__photo" />
+            <button
+              class="review-form__photoDeleteButton"
+              type="button"
+            ></button>
+          </div>
+        </div>
+      </form>
+    </div>
+    <hr class="review__decorateLine" />
+    <footer class="review__footer">
+      <ActionButton text="Отправить" />
+    </footer>
+  </section>
+</template>
+
+<script>
+import ActionButton from "@/components/blocks/actionButton/ActionButton.vue";
+import StarsRate from "@/components/blocks/starsRate/StarsRate.vue";
+export default {
+  name: "review",
+  data() {
+    return {
+      authorPhotoUrl: "./img/724cropBig1.jpg",
+      starsInfo: [
+        {
+          title: "Скорость",
+          rate: 3
+        },
+        {
+          title: "Скорость отдачи видео"
+        },
+        {
+          title: "Качество"
+        },
+        {
+          title: "Пунктуальность"
+        }
+      ],
+      addedPhotos: [
+        "./img/photos/image46_0.jpg",
+        "./img/photos/image46_1.jpg",
+        "./img/photos/image46_2.jpg",
+        "./img/photos/image46_3.jpg"
+      ]
+    };
+  },
+  components: { ActionButton, StarsRate }
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/components/review/review.scss";
+</style>
